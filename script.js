46f9cebaa05bca35b9485f8b630a292dbce37439
Yuvraj Singh Solanki <111236237+yuvraj-solanki-2406@users.noreply.github.com>
2024-02-11 20:12:16 +0530

    Flip the coin

---
+++ script.js
@@ -0,0 +1,31 @@
+let front = document.getElementById("front_coin");
+let back = document.getElementById("back_coin");
+let flipSound = document.getElementById("flipSound");
+
+front.addEventListener("click", function () {
+    flipCoin();
+});
+
+back.addEventListener("click", function () {
+    flipCoin();
+});
+
+function flipCoin() {
+    flipSound.play();
+    if (Math.random() > 0.5){
+        back.style.display = "flex";
+        front.style.display = "none";
+        back.classList.add("animation_rotate");
+        setTimeout(() => {
+            back.classList.remove("animation_rotate");
+        }, 800);
+    } else {
+        front.style.display = "flex";
+        back.style.display = "none";
+        front.classList.add("animation_rotate");
+        setTimeout(() => {
+            front.classList.remove("animation_rotate");
+        }, 800);
+    }
+
+}
